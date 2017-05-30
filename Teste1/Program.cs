24edbda7dd6b5fd3fe98db5b908cc981426722c2
Unknown <dsxedx.hermanos@gmail.com>
2017-05-30 10:17:41 -0300

    sala de provas II
    Retirando espaço em branco e acrescentando os zeros

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -15,7 +15,7 @@ class ReadFromFile
         // Example #2
         // Leia cada linha do arquivo em uma matriz de seqüência de caracteres. Cada elemento
         //Da matriz é uma linha do arquivo.
-        string[] lines = File.ReadAllLines(@"E:\Desktop\CTI\Sala de provas\gararitos errados\3º ano C.dat");
+        string[] lines = File.ReadAllLines(@"E:\Desktop\CTI\Sala de provas\BTI1MA.TXT");
 
 
         // Exibir o conteúdo do arquivo usando um loop foreach.
@@ -23,42 +23,44 @@ class ReadFromFile
         {
             // Use uma guia para recuar cada linha do arquivo.
             //Console.WriteLine("\t" + line);
+            string espaco = line.Replace("	", "");
             
-            var aux = Regex.Match(line.Substring(0, 7), @"\d+");
+            var aux = Regex.Match(espaco.Substring(0, 10), @"\d+");
             var qtd = aux.Length;
             try
             {
                 //Abrir o arquivo
-                StreamWriter valor = new StreamWriter("E:\\Desktop\\CTI\\Sala de provas\\gabaritos transformados\\3º ano C.dat", true, Encoding.ASCII);
+                StreamWriter valor = new StreamWriter("E:\\Desktop\\CTI\\Sala de provas\\BTI1MAB.TXT", true, Encoding.ASCII);
                 switch (qtd)
                 {
-                    case 1:
-                        Console.WriteLine("000000" + line.Substring(0, 1) + line.Substring(7, 93));
-                        valor.WriteLine("000000" + line.Substring(0, 1) + line.Substring(7, 93));
-                        break;
-                    case 2:
-                        Console.WriteLine("00000" + line.Substring(0, 2) + line.Substring(7, 93));
-                        valor.WriteLine("00000" + line.Substring(0, 2) + line.Substring(7, 93));
-                        break;
-                    case 3:
-                        Console.WriteLine("0000" + line.Substring(0, 3) + line.Substring(7, 93));
-                        valor.WriteLine("0000" + line.Substring(0, 3) + line.Substring(7, 93));
-                        break;
                     case 4:
-                        Console.WriteLine("000" + line.Substring(0, 4) + line.Substring(7, 93));
-                        valor.WriteLine("000" + line.Substring(0, 4) + line.Substring(7, 93));
+                        Console.WriteLine("000000" + espaco.Substring(0, 1) + espaco.Substring(1, 93));
+                        valor.WriteLine("000000" + espaco.Substring(0, 1) + espaco.Substring(1, 93));
+                       // string correctString = errString.Replace("docment", "document");
                         break;
                     case 5:
-                        Console.WriteLine("00" + line.Substring(0, 5) + line.Substring(7, 93));
-                        valor.WriteLine("00" + line.Substring(0, 5) + line.Substring(7, 93));
+                        Console.WriteLine("00000" + espaco.Substring(0, 2) + espaco.Substring(2, 93));
+                        valor.WriteLine("00000" + espaco.Substring(0, 2) + espaco.Substring(2, 93));
                         break;
                     case 6:
-                        Console.WriteLine("0" + line.Substring(0, 6) + line.Substring(7, 93));
-                        valor.WriteLine("0" + line.Substring(0, 6) + line.Substring(7, 93));
+                        Console.WriteLine("0000" + espaco.Substring(0, 3) + espaco.Substring(3, 93));
+                        valor.WriteLine("0000" + espaco.Substring(0, 3) + espaco.Substring(3, 93));
+                        break;
+                    case 7:
+                        Console.WriteLine("000" + espaco.Substring(0, 4) + espaco.Substring(4, 93));
+                        valor.WriteLine("000" + espaco.Substring(0, 4) + espaco.Substring(4, 93));
+                        break;
+                    case 8:
+                        Console.WriteLine("00" + espaco.Substring(0, 5) + espaco.Substring(5, 93));
+                        valor.WriteLine("00" + espaco.Substring(0, 5) + espaco.Substring(5, 93));
+                        break;
+                    case 9:
+                        Console.WriteLine("0" + espaco.Substring(0, 6) + espaco.Substring(6, 93));
+                        valor.WriteLine("0" + espaco.Substring(0, 6) + espaco.Substring(6, 93));
                         break;
                     default:
-                        Console.WriteLine(line.Substring(0, 7) + line.Substring(7, 93));
-                        valor.WriteLine(line.Substring(0, 7) + line.Substring(7, 93));
+                        Console.WriteLine(espaco.Substring(0, 7) + espaco.Substring(7, 93));
+                        valor.WriteLine(espaco.Substring(0, 7) + espaco.Substring(7, 93));
                         break;
                 }
                 //Fecha o arquivo
